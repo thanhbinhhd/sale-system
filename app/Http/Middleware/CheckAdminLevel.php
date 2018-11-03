01f5a9bb4e29ad3e4497f49996ba4a4b4fc273f2
Le Cong Hau <anhlahau.hl@gmail.com>
2018-11-03 08:10:35 +0700

    Add middleware

---
+++ app/Http/Middleware/CheckAdminLevel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class CheckAdminLevel
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::guard('admin')->user()->level != 1) {
+            return redirect()->route('admin.home');
+        }
+        return $next($request);
+    }
+}
