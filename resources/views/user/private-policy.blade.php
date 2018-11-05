a7c6463a760b18e94fe8ad6bebfae1b8cd988b58
Minh Quang <quanghm@haposoft.com>
2018-11-05 09:43:56 +0700

    add private policy for app

---
+++ resources/views/user/private-policy.blade.php
@@ -0,0 +1,45 @@
+@extends('master')
+@section('content')
+    <span>
+        Điều khoản sử dụng
+Với việc sử dụng Mazii, bao gồm phiên bản trình duyệt và các phiên bản dành cho điện thoại di động, bạn đồng ý chấp nhận các điều khoản nêu dưới đây. Những điều khoảng này có hiệu lực ngay khi bạn lần đầu tiên dùng Mazii.
+
+Sử dụng Mazii
+
+Bạn đồng ý dùng Mazii chỉ với mục đích hỗ trợ, tham khảo và không xâm phạm quyền, giới hạn hoặc ngăn cấm người khác sử dụng Mazii.
+
+Bản quyền trí tuệ
+
+Tất cả các bản quyền, mẫu mã, quyền về kiểu mẫu, bằng sáng chế và các quyền sở hữu trí tuệ khác (đăng ký cũng như không đăng ký) trên và trong Mazii và tất cả các nội dung (dưới mọi hình thức) được đặt trên mạng đều thuộc quyền sở hữu của Mazii hoặc người cấp phép cho Mazii. Bạn không được sao chép, tái bản, cắt xén, tải xuống, chuyển tải, hoặc dùng nội dung của Mazii bất cứ trong trường hợp nào, ngoại trừ cho mục đích cá nhân và không mang tính chất thương mại. Nếu dùng nội dung của Mazii cho những mục đích khác, bạn phải được Mazii cho phép trước bằng văn bản.
+
+Thu thập thông tin
+
+Khi bạn sử dụng Mazii, chúng tôi sẽ thu thập thông tin của bạn. Bao gồm, địa chỉ ip, thông tin tài khoản bạn cung cấp như email và mật khẩu, thông tin về thiết bị di động như mã thiết bị, mã quảng cáo. Khi bạn thanh toán, mua vật phẩm trong ứng dụng, chúng tôi có thể lưu lại thông tin về tài khoản và giao dịch của bạn. Chúng tôi luôn cố gắng nhất có thể để đảm bảo các thông tin của bạn được an toàn.
+
+Đóng góp nội dung cho Mazii
+
+Tất các các nội dung do người dùng đóng góp, sẽ được chúng tôi sử dụng để chia sẻ cho các người dùng khác. Các nội dung liên quan tới chính trị, phân biệt chủng tộc, văn hoá phẩm đồi truỵ, vi phạm thuần phong mỹ tục sẽ bị xoá bỏ mà không cần thông báo trước. Trường hợp các nội dung được đóng góp vi phạm bản quyền nội dung, chúng tôi sẽ thực hiện xoá nội dung khỏi hệ thống mà không thông báo trước khi nhận được thông báo của tác giả nội dung. Tất cả nội dung người dùng đóng góp sẽ thuộc quyền sở hữu của Mazii, Mazii được tuỳ ý sử dụng mà không cần phải thông báo trước.
+
+Nội dung góp ý:
+
+1. Không vi phạm thuần phong mỹ tục của Việt Nam.
+
+2. Không tuyên truyền chống phá nhà nước, đảng phái, chính quyền.
+
+3. Không làm ảnh hưởng đến uy tín, danh dự của tổ chức cá nhân.
+
+4. Không viết những câu như: Từ này nghĩa là, nghĩa là mà viết trực tiếp nghĩa của từ vào phần góp ý.
+
+5. Khi số lượng "Không thích" của một góp ý lên đến 10 (tức là có 10 người không đồng ý với ý kiến góp ý đó), góp ý đó sẽ tự động bị xoá khỏi hệ thống.
+
+6. Khi góp ý vi phạm các điều ở trên sẽ bị xoá ngay khỏi hệ thống mà không cần báo trước. Nếu thành viên góp ý vi phạm nhiều lần sẽ bị xoá tài khoản mà không cần báo trước.
+
+Miễn trừ trách nhiệm
+
+Mazii hoạt động như một dịch vụ hỗ trợ người học tiếng Nhật và không phải là phương pháp giáo dục thay thế phương pháp giáo dục khác. Chúng tôi tuyên bố không chịu trách nhiệm với các nội dung được đăng tải trên hệ thống của chúng tôi, bao gồm cả nội dung chúng tôi đề xuất và nội dung đóng góp của người dùng. Chúng tôi luôn cố gắng hết sức để đảm bảo hệ thống luôn sẵn sàng phục vụ người dùng, trường hợp chúng tôi nâng cấp, bảo trì, hoặc đóng cửa hệ thống sẽ không cần phải báo trước. Tất cả người dùng tự chịu trách nhiệm về việc bảo quản thông tin tài khoản cá nhân của mình.
+
+Thay đổi chính sách
+
+Chính sách này có thể được thay đổi mà không cần phải thông báo trước cho bạn.
+    </span>
+    @endsection
